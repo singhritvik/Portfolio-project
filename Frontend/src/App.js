21bbd2cb0d479169466d120f23e3b20dc64984bf
singhritvik <142486943+singhritvik@users.noreply.github.com>
2024-07-04 09:27:34 +0530

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -30,7 +30,7 @@ function App() {
         </div>
         <div className="footer pb-3 ms-3">
           <Tada>
-            <h4 className="text-center">Made With ❤️ By Rakesh &copy; 2023</h4>
+            <h4 className="text-center">Made With ❤️ By Ritvik &copy; 2022</h4>
           </Tada>
         </div>
       </div>
